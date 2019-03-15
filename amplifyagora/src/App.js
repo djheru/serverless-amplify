54e4183cb29d6b4073e5849d983df62c659a14a1
Philip Damra <pdamra@dealerinspire.com>
2019-03-15 14:31:42 -0700

    Customizing withAuthenticator theme

--- amplifyagora/src/App.js
+++ amplifyagora/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withAuthenticator } from 'aws-amplify-react';
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
 import "./App.css";
 
 class App extends React.Component {
@@ -10,4 +10,24 @@ class App extends React.Component {
   }
 }
 
-export default withAuthenticator(App);
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: '#f0c0cb'
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: 'var(--amazonOrange)'
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '10px'
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)'
+  }
+};
+
+export default withAuthenticator(App, true, [], null, theme);

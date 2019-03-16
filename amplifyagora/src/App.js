10dc86e1f640f477863629788d3719d7a84897ed
Philip Damra <pdamra@dealerinspire.com>
2019-03-15 23:56:54 -0700

    Use Authenticator component to customize our navbar, set up routes

--- amplifyagora/src/App.js
+++ amplifyagora/src/App.js
@@ -1,12 +1,71 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Auth, Hub } from 'aws-amplify';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from './pages/MarketPage';
 import "./App.css";
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    this.getUserData();
+    Hub.listen('auth', this, 'onHubCapsule')
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user }) : this.setState({ user: null });
+  }
+
+
+  onHubCapsule = capsule => {
+    const { payload: { event = '' } = {} } = capsule;
+    switch(event) {
+      case 'signIn':
+        console.log('signIn');
+        this.getUserData();
+        break;
+      case 'signUp': 
+        console.log('signUp');
+        break;
+      case 'signOut': 
+        console.log('signOut');
+        this.setState({ user: null })
+        break;
+      default:
+        console.log(event);
+        return;
+    }
+  }
+
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (e) {
+      console.error('Error signing out user', e);
+    }
+  }
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+    return !user ? (<Authenticator theme={theme}/>) : (
+      <Router>
+        <React.Fragment>
+          <Navbar  user={user} handleSignout={this.handleSignout}/>
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/markets/:marketId" component={({ match: { params: { marketId = '' } = {} } = {} }) => <MarketPage marketId={marketId}  />} />
+          </div>
+        </React.Fragment>
+      </Router>
+    );
   }
 }
 
@@ -30,4 +89,5 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+export default App;

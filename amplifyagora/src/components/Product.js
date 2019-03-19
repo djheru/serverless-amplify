6557341f639b904a267e0de7e04eaf708d9e12ae
Philip Damra <pdamra@dealerinspire.com>
2019-03-18 21:45:04 -0700

    Basic update product functionality - no subscr

--- amplifyagora/src/components/Product.js
+++ amplifyagora/src/components/Product.js
@@ -2,16 +2,52 @@ import React from "react";
 import { S3Image } from 'aws-amplify-react';
 // prettier-ignore
 import { Notification, Popover, Button, Dialog, Card, Form, Input, Radio } from "element-react";
-import { convertCentsToDollars } from '../utils';
+import { convertCentsToDollars, convertDollarsToCents } from '../utils';
+import { API, graphqlOperation } from 'aws-amplify';
 import { UserContext } from '../App';
 import PayButton from './PayButton';
+import { updateProduct } from "../graphql/mutations";
 
 class Product extends React.Component {
-  state = {};
+  state = {
+    updateProductDialog: false,
+    description: '',
+    price: '',
+    shipped: false
+  };
+
+  handleUpdateProduct = async productId => {
+    try {
+       this.setState({ updateProductDialog: false});
+       const { description, price, shipped } = this.state;
+       const input = {
+         id: productId,
+         description,
+         shipped,
+         price: convertDollarsToCents(price)
+       };
+       const result = await API.graphql(graphqlOperation(updateProduct, { input }));
+       console.dir(result);
+       Notification({
+         title: "Success!",
+         message: "You did the thing!",
+         type: "success"
+       })
+    } catch (e) {
+      console.error(e);
+    }
+  }
   
   render() {
 
     const { product } = this.props;
+    const { 
+      updateProductDialog,
+      description,
+      price,
+      shipped,
+    } = this.state;
+
     console.log(product)
     return (
       <UserContext.Consumer>
@@ -41,6 +77,75 @@ class Product extends React.Component {
                     </div>
                   </div>
                 </Card>
+                <div className="text-center">
+                  { isProductOwner ? (
+                    <>
+                      <Button 
+                        type="warning"
+                        icon="edit"
+                        className="m-1"
+                        onClick={() => this.setState({ 
+                          updateProductDialog: true,
+                          description: product.description,
+                          shipped: product.shipped,
+                          price: convertCentsToDollars(product.price)
+                        })}>
+                        </Button>
+                      <Button 
+                        type="danger"
+                        icon="delete"
+                        className="m-1"
+                        onClick={() => this.setState({ updateProductDialog: true })}>
+                        </Button>
+                    </>
+                  ) : null}
+                </div>
+
+                <Dialog
+                  title="Update Product"
+                  size="large"
+                  customClass="dialog"
+                  visible={updateProductDialog}
+                  onCancel={() => this.setState({ updateProductDialog: false })}>
+                  <Dialog.Body>
+                    <Form labelPosition="top">
+                    <Form.Item label="Edit product description">
+                      <Input 
+                        type="text"
+                        icon="information"
+                        placeholder="Description"
+                        value={description}
+                        onChange={description => this.setState({ description }) }/>
+                    </Form.Item>
+
+                    <Form.Item label="Edit product price">
+                      <Input 
+                        type="number"
+                        icon="plus"
+                        placeholder="Price ($USD)"
+                        value={price}
+                        onChange={price => this.setState({ price }) }/>
+                    </Form.Item>
+
+                    <Form.Item label="Is the product shipped or emailed to the customer?">
+                      <div className="text-center">
+                        <Radio
+                          value="Emailed"
+                          checked={shipped === false}
+                          onChange={() => this.setState({ shipped: false })} />
+                        <Radio
+                          value="Shipped"
+                          checked={shipped === true}
+                          onChange={() => this.setState({ shipped: true })} />
+                      </div>
+                    </Form.Item>
+                    </Form>
+                  </Dialog.Body>
+                  <Dialog.Footer>
+                    <Button onClick={() => this.setState({ updateProductDialog: false })}>Cancel</Button>
+                    <Button type="primary" onClick={() => this.handleUpdateProduct(product.id)}>Update</Button>
+                  </Dialog.Footer>
+                </Dialog>
               </div>
             );
           }
